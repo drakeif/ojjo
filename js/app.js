729e06ab484fb3f89f4e96082f4b90a1bc5414ae
volodymyr <v.ohlopkov100@gmail.com>
2021-09-23 23:46:55 +0300

    v 0.3
    body - overflow: hidden was added

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
 const btn = document.querySelector('.header__btn'),
       menu = document.querySelector('.menu'),
-      closeMenu = document.querySelectorAll('.menu__nav');
+      closeMenu = document.querySelectorAll('.menu__nav'),
+      body = document.querySelector('#body');
 
 btn.addEventListener('click', (e) => {
     e.target.classList.toggle('active');
     menu.classList.toggle('active');
+    body.classList.toggle('active');
 });
 
 closeMenu.forEach(el => el.addEventListener('click', () => {
     btn.classList.remove('active');
     menu.classList.remove('active');
+    body.classList.remove('active');
 }));
 
 
